de4acd564e1c10d1e54f08df4a9069b78c67d549
plutphil <47215043+plutphil@users.noreply.github.com>
2022-08-19 12:23:46 +0200

    Add files via upload

---
+++ homeassistant-install-script.sh
@@ -0,0 +1,28 @@
+sudo apt update && sudo apt upgrade -y -qq
+
+
+curl -fsSL get.docker.com | sh
+
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sh get-docker.sh
+
+apt-get install -y uidmap -qq
+
+sudo dockerd-rootless-setuptool.sh install
+
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.6.3
+
+sudo apt-get install jq wget curl udisks2 libglib2.0-bin network-manager dbus -y -qq
+
+wget https://github.com/home-assistant/os-agent/releases/download/1.3.0/os-agent_1.3.0_linux_aarch64.deb
+sudo dpkg -i os-agent_1.3.0_linux_aarch64.deb
+
+wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
+sudo dpkg -i homeassistant-supervised.deb
+
+#important for unsupported systems
+ha jobs options --ignore-conditions healthy
